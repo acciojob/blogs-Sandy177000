fd09bf8896d0ced600fa83acf98e0ba7f9906600
Sandy177000 <112424645+Sandy177000@users.noreply.github.com>
2023-12-16 21:46:08 +0530

    3

--- src/main/java/com/driver/controller/ImageController.java
+++ src/main/java/com/driver/controller/ImageController.java
@@ -1,11 +1,12 @@
-package com.driver.controller;
+
+        package com.driver.controller;
 
 
-import com.driver.services.ImageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+        import com.driver.services.ImageService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/images")
